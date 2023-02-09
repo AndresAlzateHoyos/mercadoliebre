26a6e05d30fac5a21d611e13b50801639bb52f87
AndresAlzateHoyos <andresalzate.trad@gmail.com>
2023-02-09 10:05:50 -0500

    Primera actualización

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath))
 
-app.listen(3000, () => console.log());
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Server started on http://localhost:${port}')
+});
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "./views/home.html"))
